4f087f13445a625de0722b8987e489324b46259c
nikitayashny <nikita.yashny@gmaile.com>
2025-01-12 18:02:21 +0300

    change username constraint

--- src/main/java/com/yashny/realestate_backend/entities/User.java
+++ src/main/java/com/yashny/realestate_backend/entities/User.java
@@ -12,7 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String username;
 
     private String password;
